0dea1387a115043fb70c1f13bd95d040a60b313b
InsaneBlue <469045872@qq.com>
2018-12-12 23:59:50 +0800

    Site updated: 2018-12-12 23:59:49

--- js/src/utils.js
+++ js/src/utils.js
@@ -337,3 +337,16 @@ $(document).ready(function () {
   }
 
 });
+
+
+const api = new api();
+let user, friends, photo;
+api.getUser().then(returnedUser => {
+  user = returnedUser;
+  api.getFriends(user.id).then(returnedFriends) {
+    friend = returnedFriends;
+    api.getPhoto(user.id).then(photo => {
+      photo = photo;
+    })
+  }
+})
